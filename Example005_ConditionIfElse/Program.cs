c8bb82a57a9f151e3746f9d3f164ceea85bb08d0
Семен <marbledrop@yandex.ru>
2023-09-21 23:38:50 +0300

    Add Example005 ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") //ToLower() - переводит символы вводимых слов в нижний регистр
+{
+     Console.WriteLine("Ура, Это же Маша!");
+}
+else
+{
+     Console.Write("Привет, ");
+     Console.WriteLine(username);
+}
